0c0e7d3b37f59b7ed36202c84bf81ef573158812
sbylemans <sander.bylemans@continuum.be>
2021-02-01 20:12:39 +0100

    Listen to child changes

--- dist/vl-tabs.js
+++ dist/vl-tabs.js
@@ -56,40 +56,91 @@ export class VlTabs extends vlElement(HTMLElement) {
     this.__registerActiveTabListeners();
     vl.tabs.dress(this.shadowRoot);
     this.__updateActiveTab(this.getAttribute('data-vl-active-tab'));
+    this._observer = this.__observeChildElements((mutation) => {
+      mutation.forEach((m) => {
+        m.addedNodes.forEach((node) => {
+          this.__updateObservedTabs(node);
+        });
+      });
+    });
+  }
+
+  disconnectedCallback() {
+    this._observer.disconnect();
+  }
+
+  __observeChildElements(callback) {
+    const observer = new MutationObserver(callback);
+    observer.observe(this,
+        {childList: true, attributes: false, subtree: false});
+    return observer;
+  }
+
+  __updateObservedTabs(node) {
+    // get location of tab
+    const i = [...this.__tabPanes]
+        .map((tabPane) => tabPane.getAttribute('data-vl-id'))
+        .indexOf(node.getAttribute('data-vl-id'));
+
+    const tabPaneElement = this.__tabPaneElement(this.__tabPanes[i]);
+    const listElement = this.__tabListElement(this.__tabPanes[i]);
+    // add event listener to new tab for active-tab change event
+    this.__tabEventListener(tabPaneElement);
+
+    if (i === this.__tabPanes.length - 1) {
+      this.__tabList.appendChild(listElement);
+      this.__tabs.appendChild(tabPaneElement);
+    } else {
+      // i = location where new tab needs to be, where currently a tab is.
+      this.__tabList.insertBefore(listElement,
+          this.__tabList.children[i]);
+      // i+1 because of tabsWrapper
+      this.__tabs.insertBefore(tabPaneElement,
+          this.__tabs.children[i+1]);
+    }
+
+    vl.tabs.dress(this.shadowRoot);
   }
 
   _renderTabs() {
     this.__tabList.innerHTML = '';
     [...this.__tabPanes].forEach((tp) => {
-      const title = tp.getAttribute('data-vl-title');
-      const id = tp.getAttribute('data-vl-id');
-      const pathname = window.location.pathname;
-      this.__tabList.appendChild(this._template(`
+      this.__tabList.appendChild(this.__tabListElement(tp));
+    });
+  }
+
+  __tabListElement(tp) {
+    const title = tp.getAttribute('data-vl-title');
+    const id = tp.getAttribute('data-vl-id');
+    const pathname = window.location.pathname;
+    return this._template(`
         <li class="vl-tab">
           <a class="vl-tab__link" 
             href="${pathname}#${id}" 
             id="${id}" 
             data-vl-tab role="tab" >${title}</a>
         </li>
-      `));
-    });
+      `);
   }
 
   _renderSections() {
-    [...this.__tabPanes].forEach((tp, index) => {
-      const id = tp.getAttribute('data-vl-id');
-      tp.setAttribute('slot', id + '-' + index);
+    [...this.__tabPanes].forEach((tp) => {
+      this.__tabs.appendChild(this.__tabPaneElement(tp));
+    });
+  }
 
-      this.__tabs.appendChild(this._template(`
+  __tabPaneElement(tp) {
+    const id = tp.getAttribute('data-vl-id');
+    tp.setAttribute('slot', id+'-slot');
+    return this._template(`
         <section class="vl-col--1-1 vl-tab__pane" 
           data-vl-tab-pane tabindex="0" 
           role="tabpanel" 
           id="${id}-pane" 
           hidden="hidden">
-          <slot name="${id}-${index}"></slot>
+          <slot name="${id}-slot"></slot>
         </section>
-      `));
-    });
+      `);
   }
 
   __updateActiveTab(activeTab) {
@@ -102,12 +153,16 @@ export class VlTabs extends vlElement(HTMLElement) {
 
   __registerActiveTabListeners() {
     this.shadowRoot.querySelectorAll('[data-vl-tab]').forEach((tb) => {
-      tb.addEventListener('click', () => {
-        if (this.getAttribute('data-vl-active-tab') !== tb.id) {
-          this.dispatchEvent(new CustomEvent('change',
-              {detail: {activeTab: tb.id}, bubbles: true, composed: true}));
-        }
-      });
+      this.__tabEventListener(tb);
+    });
+  }
+
+  __tabEventListener(tb) {
+    tb.addEventListener('click', () => {
+      if (this.getAttribute('data-vl-active-tab') !== tb.id) {
+        this.dispatchEvent(new CustomEvent('change',
+            {detail: {activeTab: tb.id}, bubbles: true, composed: true}));
+      }
     });
   }
 

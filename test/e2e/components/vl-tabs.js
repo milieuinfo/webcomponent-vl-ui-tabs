3918c620eb91c20fb211e02f85a1a7411815d847
Tom Coemans <tom.coemans@gmail.com>
2021-02-02 11:56:02 +0100

    added e2e test

--- test/e2e/components/vl-tabs.js
+++ test/e2e/components/vl-tabs.js
@@ -1,5 +1,21 @@
 const {VlElement} = require('vl-ui-core').Test;
+const {By} = require('vl-ui-core').Test.Setup;
+const VlTab = require('./vl-tab');
 
-class VlTabs extends VlElement {}
+class VlTabs extends VlElement {
+  async getTabs() {
+    const elements = await this._getTabElements();
+    return Promise.all(elements.map((element) => new VlTab(this.driver, element)));
+  }
+
+  async _getTabListElement() {
+    return this.shadowRoot.findElement(By.css('#tabList'));
+  }
+
+  async _getTabElements() {
+    const list = await this._getTabListElement();
+    return list.findElements(By.css('.vl-tab'));
+  }
+}
 
 module.exports = VlTabs;
--- test/e2e/tabs.test.js
+++ test/e2e/tabs.test.js
@@ -9,7 +9,12 @@ describe('vl-tabs', async () => {
     return vlTabsPage.load();
   });
 
-  it('', async () => {
-    assert.isTrue(true);
+  it('als gebruiker kan ik de verschillende tabs bekijken', async () => {
+    const tabs = await vlTabsPage.getTabs();
+    const tabElements = await tabs.getTabs();
+    assert.lengthOf(tabElements, 3);
+    await assert.eventually.equal(tabElements[0].getText(), 'Trein');
+    await assert.eventually.equal(tabElements[1].getText(), 'Metro, tram en bus');
+    await assert.eventually.equal(tabElements[2].getText(), 'Fiets');
   });
 });

bcc27d7a3b2a015d312a30fc9c4f0a5132a6d1bd
Tom Coemans <tom.coemans@gmail.com>
2021-02-25 20:19:07 +0100

    dist build

---
+++ dist/vl-tabs-all.src.js
@@ -0,0 +1,12 @@
+import {VlTab} from '../dist/vl-tab.src.js';
+import {VlTabs} from '../dist/vl-tabs.src.js';
+import {VlTabsPane} from '../dist/vl-tabs-pane.src.js';
+import {VlTabSection} from '../dist/vl-tab-section.src.js';
+
+export {
+  VlTab,
+  VlTabs,
+  VlTabsPane,
+  VlTabSection,
+};
+
